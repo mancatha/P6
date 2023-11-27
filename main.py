fd5b14fc7ed887ff8f87335de426d9ca0bc18b56
mancatha <benmanks2015@gmail.com>
2023-11-27 11:03:12 +0000

    app

--- main.py
+++ main.py
@@ -1,6 +1,52 @@
 from fastapi import FastAPI
-import unicorn
-from typing import List,Literal
+import joblib
 from pydantic import BaseModel
-import pandas as pd 
-import joblib,os
+import pandas as pd
+
+pipeline = joblib.load('./model/rf_pl.joblib')
+encoder = joblib.load('./model/label_encoder.joblib')
+
+
+app = FastAPI()
+
+class SepsisFeatures(BaseModel):
+    PRG: int
+    PL: int
+    PR:int
+    SK :int
+    TS :int
+    M11:float
+    BD2:float
+    Age:int
+    Insurance:int
+
+@app.get('/')
+def home():
+    return "Hello world"
+
+@app.get('/info')
+def appinfo():
+    return 'This is the info page of this app'
+ 
+ 
+# Define the prediction endpoint
+@app.post('/predict_sepsis')
+def predict_sepsis(sepsis_features: SepsisFeatures):
+    # Convert input features to a DataFrame
+    df = pd.DataFrame([sepsis_features.model_dump()])
+
+    # Perform prediction using the pre-trained pipeline
+    prediction = pipeline.predict(df)
+
+    encoder_prediction = encoder.inverse_transform([prediction])[0]
+    
+
+    return {'prediction' : encoder_prediction}
+
+
+
+
+
+
+    
+#return response
